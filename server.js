9796b700a3917749a8cc623d4af496c59715df76
Byron Rosas <byron_mma@hotmail.es>
2020-01-19 15:44:51 -0500

    delete corsoptions

--- server.js
+++ server.js
@@ -10,22 +10,10 @@ const express = require('express');
 const app = express();
 
 const port = process.env.PORT || 3001;
-const FRONTEND_URL = process.env.FRONTEND_URL | "http://localhost:3000";
-var whitelist = [FRONTEND_URL]
-const corsOptions = {
-  origin:  function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
-    }
-  },
-  credentials: true
-}
 
 //MIDDLEWARES
 app.use(bodyParser.json()) // for parsing application/json
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(helmet.frameguard({ action: 'sameorigin' }))
 
 runAPI(app);
